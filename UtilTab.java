ebbe78ac650880cfdb647f73c4c92e716ad74092
Ablaye11 <seckablaye519@gmail.com>
2024-11-07 17:44:06 +0000

    Changement UtilTab.java

--- UtilTab.java
+++ UtilTab.java
@@ -1,7 +1,5 @@
-// Fichier UtilTab.java
 public class UtilTab {
 
-    // Méthode statique somme
     public static double somme(double[] tab) {
         double sum = 0;
         for (double val : tab) {
@@ -10,22 +8,18 @@ public class UtilTab {
         return sum;
     }
 
-    // Méthode statique incre
     public static void incre(double[] tab, double valeur) {
         for (int i = 0; i < tab.length; i++) {
             tab[i] += valeur;
         }
     }
 
-    // Méthode statique pour afficher un tableau
     public static void affiche(double[] tab) {
         for (double val : tab) {
             System.out.print(val + " ");
         }
         System.out.println();
     }
-
-    // Méthode main pour tester la classe
     public static void main(String[] args) {
         double[] tab = {1.0, 2.0, 3.0, 4.0};
         System.out.println("Somme: " + UtilTab.somme(tab));
