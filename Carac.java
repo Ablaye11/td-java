e4c5790c511714b46a975e6e5a35cd3f66e0cf8e
Ablaye11 <seckablaye519@gmail.com>
2024-11-07 17:56:00 +0000

    Carac.java

--- Carac.java
+++ Carac.java
@@ -1,20 +1,12 @@
-// Fichier Carac.java
 public class Carac {
-
-    // Attribut caractère
     private char caractere;
-
-    // Constructeur avec paramètre
     public Carac(char caractere) {
         this.caractere = caractere;
     }
-
-    // Constructeur sans paramètre
     public Carac() {
         this.caractere = ' ';
     }
 
-    // Méthode estVoyelle
     public boolean estVoyelle() {
         char[] voyelles = {'a', 'e', 'i', 'o', 'u', 'y'};
         for (char v : voyelles) {
@@ -24,8 +16,6 @@ public class Carac {
         }
         return false;
     }
-
-    // Méthode main pour tester la classe
     public static void main(String[] args) {
         Carac c1 = new Carac('a');
         System.out.println("Est-ce une voyelle ? " + c1.estVoyelle());
