49dcdef5bcb544344113bc42ec8f88498658a2f9
Ablaye11 <seckablaye519@gmail.com>
2024-11-07 18:02:48 +0000

    Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,45 +1,39 @@
-// Fichier Rectangle.java
 public class Rectangle {
 
-    // Attributs largeur et hauteur
     private double largeur;
     private double hauteur;
-
-    // Constructeur sans paramètre
+    
     public Rectangle() {
         this.largeur = 1;
         this.hauteur = 1;
     }
 
-    // Constructeur avec un paramètre
+   
     public Rectangle(double dimension) {
         this.largeur = dimension;
         this.hauteur = dimension;
     }
 
-    // Constructeur avec deux paramètres
+   
     public Rectangle(double largeur, double hauteur) {
         this.largeur = largeur;
         this.hauteur = hauteur;
     }
 
-    // Méthode périmètre
     public double perimetre() {
         return 2 * (this.largeur + this.hauteur);
     }
 
-    // Méthode surface
     public double surface() {
         return this.largeur * this.hauteur;
     }
 
-    // Méthode agrandit
+
     public void agrandit(double facteur) {
         this.largeur *= facteur;
         this.hauteur *= facteur;
     }
 
-    // Méthode main pour tester la classe
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
         System.out.println("Périmètre: " + r1.perimetre());
