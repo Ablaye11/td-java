9d468ee70654fb7a20a8a99e6b5d50f0e9074cdf
Ablaye11 <seckablaye519@gmail.com>
2024-11-07 18:00:33 +0000

    HelloWorldWithMethod.java

--- HelloWorldWithMethod.java
+++ HelloWorldWithMethod.java
@@ -1,16 +1,11 @@
-// Fichier HelloWorldWithMethod.java
 public class HelloWorldWithMethod {
 
-    // Méthode Hello
     public void Hello() {
         System.out.println("You’re Welcome !");
     }
-
-    // Méthode main
+    
     public static void main(String[] args) {
-        // Instanciation de la classe
         HelloWorldWithMethod s = new HelloWorldWithMethod();
-        // Appel de la méthode Hello
         s.Hello();
     }
 }
